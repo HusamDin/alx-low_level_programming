9ef765b380f0366082d8e4d3673b6c432a176fb2
HusamDin <hossamn069@gmail.com>
2022-09-09 14:24:01 -0700

    I can't help but love Betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,4 +2,20 @@
 #include <stdlib.h>
 
 /**
-* main - Prints 
+* main - Prints the alphabet from a to z
+*
+* Description: Using the main function
+* This program prints the alphabet in lowercase from a to z
+* Return: Always 0 (Sucessful)
+*/
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter++);
+		putchar('\n');
+	}
+	return (0);
+}
