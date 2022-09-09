f437c0380183a2bdd9a2b621a06c0ac118dbfa87
HusamDin <hossamn069@gmail.com>
2022-09-08 18:53:30 -0700

    Welcome to C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
+/**
+  * main - Printing
+  *
+  * Return: Always 0 (Successful)
+  */
 int main(){
   puts("Programming is like building a multilingual puzzle");
   return 0;
