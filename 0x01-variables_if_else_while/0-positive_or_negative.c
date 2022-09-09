4fb123944696741e2f29ce7808ffd2665f57efab
HusamDin <hossamn069@gmail.com>
2022-09-09 12:55:00 -0700

    Betty is my best friend

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,8 @@
 /**
 * main - Prints a positive or negative number
 *
+* Description: Using the main function
+* this program prints whether the integer n is positive, is negative, or is zero
 * Return: 0
 */
 int main(void)
