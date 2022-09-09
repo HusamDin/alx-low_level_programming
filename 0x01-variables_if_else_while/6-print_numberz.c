cdaa4d1a1ae928406b02142c517f1185bb568d95
HusamDin <hossamn069@gmail.com>
2022-09-09 16:29:58 -0700

    Betty, Husam is in awe

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints all base-ten digits from 0 to 9
+*
+* Description: Using only the main function
+* This program prints all base-ten digits
+* Return: 0 (Succesful)
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+		putchar(i++);
+	putchar('\n');
+	return (0);
+}
