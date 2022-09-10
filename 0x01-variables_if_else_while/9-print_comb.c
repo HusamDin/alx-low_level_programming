91cb4946feb3d9c41a279bc847f5e9821c91caa2
HusamDin <hossamn069@gmail.com>
2022-09-09 17:49:46 -0700

    Betty

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,14 @@
 */
 int main(void)
 {
-	int ln, hn;
+	int ln;
 
 	for (ln = 48; ln <= 57; ln++)
 	{
-		for (hn = 48; hn <= 57; hn++)
-		{
-			putchar(ln);
-			putchar(hn);
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(ln);
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
