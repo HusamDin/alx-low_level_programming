9934797d974df2c5b55ca93e18f07812750958e0
HusamDin <hossamn069@gmail.com>
2022-09-12 17:04:58 -0700

    That was awesome

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - Prints all possible combinations of 2 digits
+*
+* Return: 0
+*/
+int main(void)
+{
+	int ln, hn;
+
+	for (ln = 48; ln <= 57; ln++)
+	{
+		for(hn = 49; hn <= 57; hn++)
+		{
+			if (hn > ln)
+			{
+				putchar(ln);
+				putchar(hn);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
