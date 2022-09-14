933926da578f377287dab649d2e169c237bfbb3b
HusamDin <hossamn069@gmail.com>
2022-09-14 16:49:10 -0700

    That was cool

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -7,12 +9,22 @@
  */
 int main(void)
 {
+
+
+
     int r;
+
+    int n = INT_MIN;
 
     print_last_digit(98);
     print_last_digit(0);
+    print_last_digit(INT_MIN);
     r = print_last_digit(-1024);
     _putchar(48 + r);
     _putchar('\n');
+    if (n > 0 || n == 0)
+	    printf("%d\n", n);
+    else
+	    printf("Hello, World\n");
     return (0);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,14 @@
 
 int print_last_digit(int n)
 {
-	if (n > 0 || n == 0)
+	if ((n % 10) > 0 || (n % 10) == 0)
 	{
 		_putchar('0' + n % 10);
 		return (n % 10);
 	}
 	else
 	{
-		_putchar('0' + (n * -1) % 10);
+		_putchar('0' + ((n % 10) * -1));
 		return ((n * -1) % 10);
 	}
 }
