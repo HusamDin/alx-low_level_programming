231d20558f2b2cc97f0be1ef04b124b50cd1a6e7
HusamDin <hossamn069@gmail.com>
2022-09-14 05:36:21 -0700

    I hope

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,21 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - Prints _putchar
- * @msg: The array of characters to print
+ * main - Prints _putchar
  *
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
- */
-int _putchar(char *msg)
-{
-	return (write(1, msg, 10));
-}
-
-/**
- * main - Calling _putchar function
- *
- * Return: 0
+ * Return: 0.
  */
 int main(void)
 {
-	char *msg = "_putchar\n";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	putchar(msg);
 	return (0);
 }
