e44976c19954162a4fe238629bc5be3f739e0151
HusamDin <hossamn069@gmail.com>
2022-09-14 21:07:22 -0700

    I am building a growth mindset

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - Adds two integers
+ * @n1: the first number to be added
+ * @n2: the second number to be added
+
+ * Return: The addition of 2 numbers
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
