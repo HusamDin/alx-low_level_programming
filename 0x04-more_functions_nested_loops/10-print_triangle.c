4222608be6002ad91827a5526087228dd18338fd
HusamDin <hossamn069@gmail.com>
2022-09-16 19:50:21 -0700

    I love a challenge

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,13 +11,15 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 0; i < size; i++)
+	if (n <= 0)
 	{
-		for (j = size - 1; j > i; j--)
-			_putchar(32);
-		for (k = 0; k <= i; k++)
-			_putchar(35);
-		_putchar(10);
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - 1; j > i; j--)
+				_putchar(32);
+			for (k = 0; k <= i; k++)
+				_putchar(35);
+			_putchar(10);
+		}
 	}
-	_putchar(10);
 }
