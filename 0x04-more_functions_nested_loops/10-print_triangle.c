92079f8283d33030a4b351c1717e1e561b45c49d
HusamDin <hossamn069@gmail.com>
2022-09-16 19:07:07 -0700

    Fizz is a champion

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle
+ * @size: The the size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = size; j > i; j--)
+			_putchar(32);
+		for (k = 0; k <= i; k++)
+			_putchar(42);
+		_putchar(10);
+	}
+	_putchar(10);
+}
