91ddeeecca87e142fb2bf906521aa33619dc230e
HusamDin <hossamn069@gmail.com>
2022-09-19 17:17:04 -0700

    I commit to be a better person

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: A pointer to a string
+ *
+ * Return: A reversed String
+ */
+
+int _strlen(char *s)
+{
+	return strlen(s);
+}
