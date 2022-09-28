245a9aa2b6aa4f83317356117c63e0525f415eb2
HusamDin <hossamn069@gmail.com>
2022-09-27 17:01:07 -0700

    I love a challenge

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -9,10 +9,10 @@
 int main(void)
 {
     char *s = "hello, world";
-    char *f = "wleh";
-    unsigned int n;
+    char *f = "hello";
+    char *t;
 
-    n = _strspn(s, f);
-    printf("%u\n", n);
+    t = _strstr(s, f);
+    printf("%s\n", t);
     return (0);
 }
