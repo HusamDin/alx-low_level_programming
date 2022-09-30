d5e9068c45a3329de091ed2fd059b265d5c396cd
HusamDin <hossamn069@gmail.com>
2022-09-30 16:48:03 -0700

    My first library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,16 @@
-void _puts(char *s);
+#include "main.h"
+/**
+ * _puts - Prints a string
+ * @s: The string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\0');
+}
