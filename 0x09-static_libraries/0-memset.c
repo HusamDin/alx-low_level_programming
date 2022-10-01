91ef44a93111f60932f7468f5eec415fb754eee5
HusamDin <hossamn069@gmail.com>
2022-10-01 05:50:34 -0700

    I love a challenge

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,1 +1,25 @@
-char *_memcpy(char *dest, char *src, unsigned int n);
+#include "main.h"
+
+/**
+ * _memset - fill n byte of memory with a constant value
+ * @s: pointer to a memory area
+ * @b: constant value
+ * @n: number of byte to fill
+ * Return: memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int size = n; /* accept +ve no. */
+
+	if (size > 0)
+	{
+		int p;
+
+		for (p = 0; p < size; p++)
+		{
+			s[p] = b;
+		}
+	}
+	return (s);
+}
--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,16 @@
-int _strlen(char *s);
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of string
+ * @s: string to find length
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int m = 0;
+
+	while (*(s + m))
+		m++;
+	return (m);
+}
