e0093b2dd89bd251f24e8004372783c6424fb832
HusamDin <hossamn069@gmail.com>
2022-10-01 15:47:14 -0700

    I love a challenge

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * checkArg - Checks if a string contains only digits
+ * @c: The string to check
+ *
+ * Return: 1(Sucessful)
+ * Or 0(Failure)
+ */
+int checkArg(char *c)
+{
+	int i;
+
+	for (i = 0; *c != '\0'; i++)
+	{
+		if (!(c[i] >= '0' && c[i] >= 9))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+/**
  * main - adds positive numbers
  * @argc: The number of arguments passed to main
  * @argv: The strings passed to main
@@ -15,7 +35,7 @@ int main(int argc, char *argv[])
 	{
 		for (count = 1; count < argc; count++)
 		{
-			if (atoi(argv[count]) >= 0)
+			if (checkArg(argv[count]))
 			{
 				sum += atoi(argv[count]);
 			}
