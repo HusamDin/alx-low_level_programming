b51de57698e2155fc5dc0bb36f0f7de839463ede
HusamDin <hossamn069@gmail.com>
2022-10-01 14:05:32 -0700

    I love ALX

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: The number of arguments passed to main
+ * @argv: The strings passed to main
+ *
+ * Return: 0(successful)
+ * Or 1(failure)
+ */
+int main(int argc, char *argv[])
+{
+	int count, mul;
+
+	if (argc == 3)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			mul *= atoi(argv[count]);
+		}
+		printf("%d\n", mul);
+       	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
