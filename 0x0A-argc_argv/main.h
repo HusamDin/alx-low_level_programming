7d915e13edccc2552ea46a4f8bd2f84891c0cc6d
HusamDin <hossamn069@gmail.com>
2022-10-01 16:07:58 -0700

    sum

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,12 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+#ifndef HOLBERTON
+#define HOLBERTON
 
 int _putchar(char c);
 
