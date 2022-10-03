f7da5f305307ae1913b1e5389fb20592762ecc2e
HusamDin <hossamn069@gmail.com>
2022-10-03 13:05:55 -0700

    My first malloc and free program

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	s = (char *)malloc(sizeof(char) * size);
+	if (s == NULL)
+		return ('\0');
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
