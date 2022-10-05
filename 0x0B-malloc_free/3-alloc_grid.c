151dacfc9d5938ab6dbb7deaa5538115a5e2f6f1
HusamDin <hossamn069@gmail.com>
2022-10-05 06:57:43 -0700

    a challenge

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,11 +22,8 @@ int **alloc_grid(int width, int height)
 		ptr[i] = malloc(sizeof(int) * width);
 		if (ptr[i] == NULL)
 		{
-			for (i = 0; i < height; i++)
-			{
-				free(ptr[i]);
-				free(ptr);
-			}
+			free(ptr[i]);
+			free(ptr);
 			return (NULL);
 		}
 	}
