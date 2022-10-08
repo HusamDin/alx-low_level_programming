92f5e2505e34b5cc1afa727a2ead2b805f907c2a
HusamDin <hossamn069@gmail.com>
2022-10-08 07:43:46 -0700

    Welcome to malloc free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,13 +9,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *c;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	c = malloc(nmemb * size);
+
 	if (c == NULL)
 		return (NULL);
 
+	for (i = 0; i < nmemb * size; i++)
+		c[i] = 0;
 	return (c);
 }
