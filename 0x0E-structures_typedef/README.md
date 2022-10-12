3a92dae23ccf65e46a551e8236373058f7522d82
HusamDin <hossamn069@gmail.com>
2022-10-12 15:22:34 -0700

    My first struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Welcome to structs and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - with 3 members
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog's owner name
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
