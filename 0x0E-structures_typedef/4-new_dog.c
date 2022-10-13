ec781a4aadf78c169f5a7c52e4a8146404786e76
HusamDin <hossamn069@gmail.com>
2022-10-13 04:47:56 -0700

    Weird

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - creates a new dog
  * @name: the dog name
@@ -11,11 +12,23 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int nameLen, ownerLen;
+
+	nameLen = strlen(name);
+	ownerLen = strlen(owner);
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
+	d->name = malloc(sizeof(name) * nameLen);
+	if (d->name == NULL)
+		return (NULL);
+
+	d->owner = malloc(sizeof(owner) * ownerLen);
+	if (d->owner == NULL)
+		return (NULL);
+	
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
