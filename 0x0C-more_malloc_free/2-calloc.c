7cea2a6eab1be4aca3d960d4d58ba5937eda4c62
HusamDin <hossamn069@gmail.com>
2022-10-13 07:29:09 -0700

    back

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (c == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		c[i] = 0;
 	return (c);
 }
