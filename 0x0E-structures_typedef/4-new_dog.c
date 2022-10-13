010948908b6f3f22b75747892b4b384a3cf913c7
HusamDin <hossamn069@gmail.com>
2022-10-13 05:11:05 -0700

    Weird

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,11 +26,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d->name = malloc((sizeof(name) * nameLen) + 1);
 	if (d->name == NULL)
+	{
+		free (d);
 		return (NULL);
+	}
 
 	d->owner = malloc((sizeof(owner) * ownerLen) + 1);
 	if (d->owner == NULL)
+	{
+		free (d->name);
+		free (d);
 		return (NULL);
+	}
 
 	d->name = name;
 	d->age = age;
