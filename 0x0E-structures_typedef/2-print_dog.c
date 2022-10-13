b9f3e5d0d943ec7401565f6fd615e6b7c311a72d
HusamDin <hossamn069@gmail.com>
2022-10-12 18:45:29 -0700

    dog loyalty

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints a struct dog
+ * @d: the struct to print
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name != NULL && d->owner != NULL)
+			printf("Name: %s\nAge: %f\nOwner: %s\n", d->name,
+			       d->age, d->owner);
+		if (d->name != NULL && d->owner == NULL)
+			printf("Name: %s\nAge: %f\nOwner: (nil)\n", d->name,
+			       d->age);
+		if (d->name == NULL && d->owner != NULL)
+			printf("Name: (nil)\nAge: %f\nOwner: %s\n", d->age,
+				d->owner);
+		if (d->name == NULL && d->owner == NULL)
+			printf("Name: (nil)\nAge: %f\nOwner: (nul)\n", d->age);
+	}
+}
