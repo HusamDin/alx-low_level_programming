5477dccc8de7f9a1f35fcb620f6ecb10fd5cf43f
HusamDin <hossamn069@gmail.com>
2022-10-14 20:28:20 -0700

    loop through funcptr

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+/**
+ * array_iterator - executes a function given as a parameter on each element
+ * of an array
+ * @array: an array of integers
+ * @size: the size of the array
+ * @action: a pointer to a function to be executed on each array element
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
