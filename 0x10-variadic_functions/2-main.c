765ad30d2e8278182e68fc9980f022023ad63151
HusamDin <hossamn069@gmail.com>
2022-10-16 14:33:38 -0700

    Print strings passed to a variadic function

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+
+ * main - check the code
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+
+{
+
+	print_strings(", ", 3, "Jay", "Django", "Hoss");
+
+    return (0);
+
+}
