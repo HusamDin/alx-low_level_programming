d31c895f072edd7f79bddc1c88718ada8a24ffaa
HusamDin <hossamn069@gmail.com>
2022-10-22 07:08:39 -0700

    print a new node at the front of a linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,15 +4,17 @@
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: a pointer to the head node pointer
- * str: a node string
+ * @str: a node string
  *
  * Return: pointer to a new node if successful
  * or NULL if failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode = malloc(sizeof (list_t));
+	list_t *newNode = malloc(sizeof(list_t));
 
+	if (newMode == NULL)
+		return (NULL);
 	newNode->str = strdup(str);
 	newNode->len = strlen(str);
 	newNode->next = (*head);
