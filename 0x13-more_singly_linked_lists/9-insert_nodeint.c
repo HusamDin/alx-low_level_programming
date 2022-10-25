df9258213aa2aa169dd62c8892678c068e9749e8
HusamDin <hossamn069@gmail.com>
2022-10-25 14:00:35 -0700

    add a node at a specific index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,9 +26,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	tmp = nthNode->next;
-	nthNode->next = newNode;
 	newNode->n = n;
 	newNode->next = tmp;
+	nthNode->next = newNode;
 
 	return (newNode);
 }
