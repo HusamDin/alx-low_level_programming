5a9198c5c2cd057ddde7e6dee456345f41d1405a
HusamDin <hossamn069@gmail.com>
2023-02-09 23:15:19 -0800

    Delete a node at a specific index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,44 +2,43 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Deletes a node at any index
- * @head: A pointer to the head pointer
+ * delete_dnodeint_at_index - Deletes a node at any index of a linked list
+ * @head: A pointer to the head node pointer
  * @index: The index of the node that should be deleted
  *
- * Return: 1 if succeeded
- * Or -1 if it failed
+ * Returns: 1 if it succeeded, -1 if it failed
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp = (*head);
+	dlistint_t *bef_temp;
 	dlistint_t *aft_temp;
-	dlistint_t *aft_aft_temp;
 	unsigned int idx = 0;
 
-	if (temp == NULL)
+	if ((*head) == NULL)
 		return (-1);
 
 	if (index == 0)
 	{
-		(*head) = (*head)->next;
+		(*head) = temp->next;
 		free(temp);
 		return (1);
 	}
 
-	while (temp != NULL && idx < index - 1)
+	while (idx < index)
 	{
 		temp = temp->next;
 		idx++;
 	}
 
-	if (temp == NULL)
-		return (-1);
-
+	bef_temp = temp->prev;
 	aft_temp = temp->next;
-	aft_aft_temp = aft_temp->next;
-	temp->next = aft_temp->next;
-	aft_aft_temp->prev = temp;
-	free(aft_temp);
+
+	bef_temp->next = aft_temp;
+	aft_temp->prev = bef_temp;
+
+	free(temp);
+
 	return (1);
 }
