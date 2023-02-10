b49895ad97210379b49ea398b5225947c0f9f770
HusamDin <hossamn069@gmail.com>
2023-02-10 06:44:18 -0800

    Delete a node at a specific index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,6 +19,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if ((*head) == NULL)
 		return (-1);
 
+	if ((*head) != NULL)
+		while (temp->prev != NULL)
+			temp = temp->prev;
+
 	if (index == 0)
 	{
 		(*head) = temp->next;
