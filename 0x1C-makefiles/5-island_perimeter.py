ca87e9313daf53ca5cfea3e253298c077f9a3ecc
HusamDin <hossamn069@gmail.com>
2023-02-24 21:31:35 -0800

    Calculate the perimeter of an island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+
+This Module has one function to return the perimeter of an island
+
+"""
+
+
+def island_perimeter(grid):
+    """
+    This function returns the permiter of an island described in grid
+
+    Args:
+        grid: a list of lists of integers
+
+    Return: The perimiter of the island
+    """
+
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for value in range(len(grid[row])):
+            if grid[row][value] == 1:
+                perimeter += 4
+                if grid[row - 1][value] == 1:
+                    perimeter -= 2
+                if grid[row][value - 1] == 1:
+                    perimeter -= 2
+
+    return (perimeter)
